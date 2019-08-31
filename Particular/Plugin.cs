0cd0b853dd7271eacd191887473b4e722634c078
Jack Baron <jackmbaron@gmail.com>
2019-08-31 02:31:56 +0100

    setup plugin

--- Particular/Plugin.cs
+++ Particular/Plugin.cs
@@ -1,8 +1,52 @@
 using System;
+using IPA;
+using IPA.Logging;
+using UnityEngine.SceneManagement;
 
 namespace Particular
 {
-    public class Plugin
+    public class Plugin : IBeatSaberPlugin
     {
+        public static Logger log;
+
+        public void Init(object _, Logger logger)
+        {
+            log = logger;
+        }
+
+        public void OnApplicationStart()
+        {
+
+        }
+
+        public void OnApplicationQuit()
+        {
+
+        }
+
+        public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+
+        }
+
+        public void OnActiveSceneChanged(Scene old, Scene scene)
+        {
+
+        }
+
+        public void OnSceneUnloaded(Scene scene)
+        {
+
+        }
+
+        public void OnUpdate()
+        {
+
+        }
+
+        public void OnFixedUpdate()
+        {
+
+        }
     }
 }
