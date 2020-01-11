f564ad1d482fae657a777515064669ebca9f072c
Zingabopp <Zingabopp@gmail.com>
2020-01-11 05:51:45 -0600

    Reworked the SpawnParticles Harmony patch.

--- Particular/Patches/NoteCutParticlesEffect_SpawnParticles.cs
+++ Particular/Patches/NoteCutParticlesEffect_SpawnParticles.cs
@@ -1,5 +1,6 @@
 using System;
 using Particular.Utils;
+using Particular.Settings;
 using Harmony;
 using UnityEngine;
 
@@ -9,52 +10,19 @@ namespace Particular.Patches
     [HarmonyPatch("SpawnParticles")]
     internal class NoteCutParticlesEffect_SpawnParticles
     {
-        internal static void Prefix(ref NoteCutParticlesEffect __instance, ref int sparkleParticlesCount, ref int explosionParticlesCount)
+        internal static void Prefix(ref NoteCutParticlesEffect __instance, ref Color32 color, ref int sparkleParticlesCount, ref int explosionParticlesCount, ref float lifetimeMultiplier,
+            ref ParticleSystem[] ____sparklesPS, ref ParticleSystem ____explosionPS)
         {
-            ParticleSystem[] sparklesPS;
-            try
-            {
-                sparklesPS = (ParticleSystem[])__instance.GetField("_sparklesPS");
-            }
-            catch (NullReferenceException)
-            {
-                Plugin.log.Critical("Failed to get _sparklesPS field!");
-                return;
-            }
-
-            ParticleSystem explosionPS;
-            try
-            {
-                explosionPS = (ParticleSystem)__instance.GetField("_explosionPS");
-            }
-            catch (NullReferenceException)
-            {
-                Plugin.log.Critical("Failed to get _explosionPS field!");
-                return;
-            }
-
-            float slashMultiplier = Plugin.config.GetFloat("particles", "slash-particles") ?? 1f;
-            float explosionMultiplier = Plugin.config.GetFloat("particles", "explosion-particles") ?? 1f;
-            float lifetimeMultiplier = Plugin.config.GetFloat("particles", "particle-lifetime") ?? 1f;
-
-            float sparkles = sparkleParticlesCount * slashMultiplier;
-            float explosion = explosionParticlesCount * explosionMultiplier;
-
-            sparkleParticlesCount = Mathf.FloorToInt(sparkles);
-            explosionParticlesCount = Mathf.FloorToInt(explosion);
-
-            foreach (ParticleSystem ps in sparklesPS)
-            {
-                ParticleSystem.MainModule psMain = ps.main;
-
-                psMain.maxParticles = int.MaxValue;
-                psMain.startLifetimeMultiplier = lifetimeMultiplier;
-            }
-
-            ParticleSystem.MainModule explosionMain = explosionPS.main;
-
+            sparkleParticlesCount = Mathf.FloorToInt(sparkleParticlesCount * Plugin.Settings.SlashParticles);
+            explosionParticlesCount = Mathf.FloorToInt(explosionParticlesCount * Plugin.Settings.ExplosionParticles);
+            lifetimeMultiplier *= Plugin.Settings.SlashParticleLifetime;
+            if (Plugin.Settings.RainbowParticles)
+                color = UnityEngine.Random.ColorHSV();
+            ParticleSystem.MainModule slashMain = ____sparklesPS[0].main;
+            slashMain.maxParticles = int.MaxValue;
+            ParticleSystem.MainModule explosionMain = ____explosionPS.main;
             explosionMain.maxParticles = int.MaxValue;
-            explosionMain.startLifetimeMultiplier = lifetimeMultiplier;
+            explosionMain.startLifetimeMultiplier = Plugin.Settings.ExplosionParticleLifetime;
         }
     }
 }
